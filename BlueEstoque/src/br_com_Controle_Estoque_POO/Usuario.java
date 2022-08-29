412bb1b6fbb9cb5b14bc12852f6b8670d900b5be
Paulojr01 <106793350+Paulojr01@users.noreply.github.com>
2022-08-28 23:13:02 -0300

    ok

--- BlueEstoque/src/br_com_Controle_Estoque_POO/Usuario.java
+++ BlueEstoque/src/br_com_Controle_Estoque_POO/Usuario.java
@@ -11,7 +11,34 @@ public class Usuario{
         this.senha_usuario = senha_usuario;
         this.cpf_usuario = cpf_usuario;
     }
-    
+
+    public void SetNome_Usuario(String nome_usuario){
+        this.nome_Usuario = nome_usuario;
+
+    }
+
+    public String GetNome_Usuario (){
+        return nome_Usuario;
+
+    }
+
+    public void SetSenha_Usuario (String senha_usuario){
+        this.senha_usuario = senha_usuario;
+    }
+
+    public String GetSenha_Usuario(){
+        return  senha_usuario;
+
+    }
+
+    public void SetCpf_Usuario (String cpf_usuario ){
+        this.cpf_usuario = cpf_usuario;
+    }
+
+    public String GetCpf_Usuario(){
+        return senha_usuario;
+
+    }
 
 }
 
