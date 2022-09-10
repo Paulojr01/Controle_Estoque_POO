8115cd7f076fc7e91fb035b10ceea5739170788c
Patrick Siqueira <psique.estuda@gmail.com>
2022-09-09 22:10:31 -0300

    criacao da classe texto e inicio do main

--- BlueEstoque/src/Main.java
+++ BlueEstoque/src/Main.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
+import br_com_Controle_Estoque_POO.Textos;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        String opcao = "";
+        Textos texto = new Textos();
+        Scanner ler = new Scanner(System.in);
+
+        do {
+            
+        
+            texto.mostrarMenuPrincipal();
+    
+            opcao = ler.next();
+    
+            switch (opcao) {
+                case "1":
+                    //Entrada de Estoque
+                    break;
+    
+                case "2":
+                // Saída de Estoque
+                    break;
+    
+                case "3":
+                // Transferência de Estoque
+                    break;
+                
+                case "4":
+                // Consulta Estoque
+                    break;
+    
+                case "5":
+                // Finalizar
+                    break;
+            
+                default:
+                    texto.mostrarInvalida();
+                    break;
+            }
+        
+        } while (  ! opcao.equals("5") ); // diferente com String
+
+        
+
     }
 }
